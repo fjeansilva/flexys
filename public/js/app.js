475283a044797a973abe6b2197f1a411b4e98b0c
Jean Silva <jeancarlos.silva@me.com>
2017-02-19 16:18:13 -0300

    done version mobile

--- public/js/app.js
+++ public/js/app.js
@@ -2,6 +2,5 @@
   document.querySelector('.header_hamburguer').addEventListener('click', function(e){
     document.querySelector('.header_menu').classList.toggle('open');
     e.stopPropagation();
-    console.log('asdfas');
   });
-});
+})();
